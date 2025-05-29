52b9fbc3f7109f8e6d1be3e0745f9cc693ea0bfd
Mike Beaumont <mjboamail@gmail.com>
2025-05-29 13:14:58 +0200

    build(deps): bump tonic from 0.12 to 0.13

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let no_includes: [&str; 0] = [];
     tonic_build::configure()
         .build_client(false)
-        .compile(&["proto/kms.proto"], &no_includes)?;
+        .build_server(true)
+        .compile_protos(&["proto/kms.proto"], &no_includes)?;
     Ok(())
 }
