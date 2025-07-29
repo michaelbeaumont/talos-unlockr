c1ccf4f47e3780b2c5146aa54388c7811b00095b
Mike Beaumont <mjboamail@gmail.com>
2025-07-29 18:53:11 +0200

    build(deps): bump tonic/prost from 0.13 to 0.14 (#30)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let no_includes: [&str; 0] = [];
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .build_client(false)
         .build_server(true)
         .compile_protos(&["proto/kms.proto"], &no_includes)?;
